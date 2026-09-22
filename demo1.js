44a15b72880a38b0cb9b02b55727df7b5e97a017
rishabh-jaiswar <rishabhjaiswar706@gmail.com>
2026-09-22 13:02:58 +0530

    updated js of login and signup

---
+++ demo1.js
@@ -0,0 +1,5 @@
+// let data=JSON.parse(localStorage.getItem("registeruer"));
+// console.log(data);
+
+ let data=JSON.parse(localStorage.getItem("registeruser"))// to read the data from localstorage  11.mrh
+            console.log(data);//
